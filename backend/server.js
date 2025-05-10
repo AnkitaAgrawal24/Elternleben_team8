979ef58d987d9ec6b4d758ac9438a6fecdebbcd8
ankitaagrawal24 <ankitaagrawal2405@gmail.com>
2025-05-10 10:37:54 +0200

    changes

--- backend/server.js
+++ backend/server.js
@@ -38,5 +38,5 @@ app.use("/api/webinars", webinarRouter);
 //
 
 // Start server
-const PORT = process.env.MOCK_API_URL || 8000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
